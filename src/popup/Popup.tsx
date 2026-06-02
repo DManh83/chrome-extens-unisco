f8df47860b68ab357afbd8fb544231396dda2fdd
gin <gin.itfanyuan@gmail.com>
2026-06-02 09:04:40 +0800

    update input

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react"
+
 function Popup() {
     const loginUrl = "https://www.unisco.com.cn/#/login"
+
+    const [blNo, setBlNo] = useState("")
+    const [carrierCode, setCarrierCode] = useState("")
 
     const start = () => {
         chrome.storage.local.set({
             started: true,
+            blNo,
+            carrierCode,
         })
 
         chrome.tabs.query(
@@ -29,7 +36,23 @@ function Popup() {
         )
     }
 
-    return <button onClick={start}>Start</button>
+    return (
+        <div
+            style={{
+                padding: 12,
+                width: 300,
+                display: "flex",
+                flexDirection: "column",
+                gap: 8,
+            }}
+        >
+            <input type='text' placeholder='BL No' value={blNo} onChange={(e) => setBlNo(e.target.value)} />
+
+            <input type='text' placeholder='Carrier Code' value={carrierCode} onChange={(e) => setCarrierCode(e.target.value)} />
+
+            <button onClick={start}>Start</button>
+        </div>
+    )
 }
 
 export default Popup
